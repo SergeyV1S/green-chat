b3d452660b34e67a82f58183bdeb51347752060f
V1S <v1s.coop@yandex.ru>
2026-09-23 18:43:24 +0500

    added credentials utils

---
+++ src/shared/utils/credentials.ts
@@ -0,0 +1,34 @@
+export type Credentials = {
+  idInstance: string;
+  apiTokenInstance: string;
+  apiUrl: string;
+};
+
+const STORAGE_KEY = "green-api-credentials";
+
+export const saveCredentials = (credentials: Credentials) => {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(credentials));
+};
+
+export const getCredentials = () => {
+  const credentials = sessionStorage.getItem(STORAGE_KEY);
+
+  if (!credentials) {
+    return null;
+  }
+
+  try {
+    return JSON.parse(credentials) as Credentials;
+  } catch {
+    return null;
+  }
+};
+
+export const clearCredentials = () => {
+  localStorage.removeItem(STORAGE_KEY);
+};
+
+export const buildRequestUrl = (
+  { apiUrl, idInstance, apiTokenInstance }: Credentials,
+  method: string
+) => `${apiUrl}/waInstance${idInstance}/${method}/${apiTokenInstance}`;
--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,1 +1,2 @@
 export * from "./createRoute";
+export * from "./credentials";
